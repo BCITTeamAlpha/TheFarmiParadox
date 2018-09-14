f514cae1a4dd80eff4305befdb7798910d531bbb
limbermuffin <limbermuffin@gmail.com>
2018-09-13 22:27:21 -0700

    MarchingSquares renamed "temp" to "vertex"

--- OpenGLTest/MarchingSquares.cpp
+++ OpenGLTest/MarchingSquares.cpp
@@ -23,7 +23,6 @@ std::vector<glm::vec3> MarchingSquares::GenerateMesh(Map m) {
 	std::vector<glm::vec3> vertexVector;
 	for (int x = 0; x < m.width() - 1; x++) {
 		for (int y = 0; y < m.height() - 1; y++) {
-
 			int index = 0;
 			if (m.isSolid(x, y)) { index += 1; }
 			if (m.isSolid(x + 1, y)) { index += 2; }
@@ -31,13 +30,12 @@ std::vector<glm::vec3> MarchingSquares::GenerateMesh(Map m) {
 			if (m.isSolid(x, y + 1)) { index += 8; }
 			for (int i = 0; i < 18; i += 2) {
 				if (_squares[index][i] == -1.0f) { break; }
-				glm::vec3 temp = glm::vec3(
+				glm::vec3 vertex = glm::vec3(
 					_squares[index][i] + x - m.width() / 2,
 					_squares[index][i + 1] + y - m.height() / 2,
 					0.0f
 				);
-
-				vertexVector.push_back(temp);
+				vertexVector.push_back(vertex);
 			}
 		}
 	}
