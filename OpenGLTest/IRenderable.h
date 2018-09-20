bb22eed0ce9d2b1727e7a00adffec713aa1ae170
limbermuffin <limbermuffin@gmail.com>
2018-09-19 18:04:06 -0700

    Added IRenderable class

---
+++ OpenGLTest/IRenderable.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <vector>
+
+#include <GL/glew.h>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+
+class IRenderable {
+public:
+	glm::vec3 _position;
+	glm::vec3 _rotation;
+	std::vector<glm::vec3> _vertices;
+	std::vector<glm::vec3> _colors;
+	std::vector<glm::vec3> _normals;
+	GLuint _vertexBufferLocation;
+	GLuint _colorBufferLocation;
+	GLuint _normalBufferLocation;
+};
