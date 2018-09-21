9fcba62bece054d047b4d68242dbde92e23fd6cc
limbermuffin <limbermuffin@gmail.com>
2018-09-20 17:26:36 -0700

    created SendToRenderer func & commented

--- OpenGLTest/main.cpp
+++ OpenGLTest/main.cpp
@@ -12,6 +12,22 @@
 #include "Map.h"
 
 std::mutex mtx;
+IRenderable empty;
+IRenderable *p = &empty;
+IRenderable **pp = &p;
+IRenderable *** const ppp = &pp;
+
+void SendToRenderer(IRenderable &renderable) {
+	while (*ppp != NULL) {
+		Sleep(1);
+	}
+	mtx.lock();
+	p = &renderable;
+	pp = &p;
+	*ppp = pp;
+	mtx.unlock();
+	std::cout << "passed IRenderable to Renderer" << std::endl;
+}
 
 std::vector<glm::vec3> quadVertices = { { 0, 5, 0 },{ 5, 5, 0 },{ 0, 0, 0 },{ 5, 0, 0 } };
 std::vector<glm::vec4> quadColors = { { 1, 0, 0, 1 },{ 1, 0, 0, 1 },{ 1, 0, 0, 1 },{ 1, 0, 0, 1 } };
@@ -19,16 +35,10 @@ std::vector<glm::vec3> quadNormals = { { 0, 0, 1 },{ 0, 0, 1 },{ 0, 0, 1 },{ 0, 
 std::vector<GLuint> quadElements = { 0, 1, 2, 2, 1, 3 };
 
 int main() {
-	IRenderable empty;
-	IRenderable *p = &empty;
-	IRenderable **pp = &p;
-	IRenderable *** const ppp = &pp;
+	// start Renderer in own thread
 	Renderer renderer = Renderer(ppp, std::ref(mtx));
-	while (*ppp != NULL) {
-		Sleep(1);
-	}
 
-
+	// setup Map IRenderable
 	std::vector<Planetoid> planets;
 	planets.push_back(Planetoid(48.0f, 88.0f, 16.0f));
 	planets.push_back(Planetoid(88.0f, 50.0f, 8.0f));
@@ -45,6 +55,7 @@ int main() {
 		map._elements.push_back(i);
 	}
 
+	// setup quad IRenderable
 	IRenderable quad;
 	quad._vertices = quadVertices;
 	quad._colors = quadColors;
@@ -52,6 +63,7 @@ int main() {
 	quad._elements = quadElements;
 	quad._position = { 64.0f, 32.0f, 0.0f };
 
+	// setup another quad IRenderable
 	IRenderable quad1;
 	quad1._vertices = quadVertices;
 	quad1._colors = quadColors;
@@ -59,43 +71,26 @@ int main() {
 	quad1._elements = quadElements;
 	quad1._position = { 64.0f, 32.0f, 0.0f };
 
-
-	mtx.lock();
-	p = &map;
-	pp = &p;
-	*ppp = pp;
-	mtx.unlock();
-	while (*ppp != NULL) {
-		Sleep(1);
-	}
-	std::cout << "passed map" << std::endl;
-
-	mtx.lock();
-	p = &quad;
-	pp = &p;
-	*ppp = pp;
-	mtx.unlock();
-	while (*ppp != NULL) {
-		Sleep(1);
-	}
-	std::cout << "passed quad" << std::endl;
-
-	mtx.lock();
-	p = &quad1;
-	pp = &p;
-	*ppp = pp;
-	mtx.unlock();
-	while (*ppp != NULL) {
-		Sleep(1);
-	}
-	std::cout << "passed quad1" << std::endl;
+	// send IRenderables to renderer
+	SendToRenderer(map);
+	SendToRenderer(quad);
+	SendToRenderer(quad1);
 
 	for (int tick = 0;; tick++) {
 		//
 		// Colt, put your stuff somewhere around here for now
 		//
-		quad._position.x += 0.1f;
-		quad1._position.y = quad._position.x;
-		Sleep(10);
+		quad1._position.x += 0.05f;
+		if (quad1._position.x > 125.5f) {
+			quad1._position.x = -2.5f;
+		}
+		quad1._position.y += 0.1f;
+		if (quad1._position.y > 125.5f) {
+			quad1._position.y = -2.5f;
+		}
+		quad._position.x = quad1._position.x + 16*sin(tick * 0.01f);
+		quad._position.y = quad1._position.y + 16*cos(tick * 0.01f);
+
+		Sleep(1000 / 59.94);
 	}
 }
