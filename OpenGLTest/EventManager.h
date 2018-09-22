8da64702a1c696f2d82f45b07a1497b3b1132d04
Matthew Taylor <mtaylor8878@gmail.com>
2018-09-22 14:39:48 -0700

    Added EventManager and Unit Tests

--- OpenGLTest/EventManager.h
+++ OpenGLTest/EventManager.h
@@ -1,9 +1,35 @@
 #pragma once
+#ifndef EVENTMANAGER_H
+#define EVENTMANAGER_H
+
 //created by main
 
+#include <map>
+#include <set>
+#include <thread>
+#include "ISubscriber.h"
+#include "EventName.h"
+
+
+/**
+    Event Manager is a static class that dispatches messages between various components of the engine.
+    Any class that implements ISubscriber can subscribe to events to be notified when that event is triggered.
+*/
 class EventManager {
 public:
-	EventManager();
-private:
+    // Use to notify subscribers of the specified event passing the given parameter
+    static void notify(EventName eventName, Param* params, bool async = true);
 
+    // Subscribes the specified subscriber to an event
+    static void subscribe(EventName eventName, ISubscriber* subscriber);
+
+    // Unsubscribes the specified subscriber from an event
+    static void unsubscribe(EventName eventName, ISubscriber* subscriber);
+private:
+    static std::map<EventName, std::set<ISubscriber*>> _eventMap;
+    static void notifySubscribers(EventName eventName, Param* params);
+
+    EventManager() {}
 };
+
+#endif
