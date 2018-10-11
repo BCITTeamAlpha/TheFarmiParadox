e76f6f38bd36b292262157c60a96e9cbe82e2af3
limbermuffin <limbermuffin@gmail.com>
2018-10-10 23:21:32 -0700

    SPAAAACE

--- OpenGLTest/main.cpp
+++ OpenGLTest/main.cpp
@@ -116,7 +116,7 @@ int main()
 	Renderable *mapSkin = new Renderable();
 	mapSkin->_z = 0;
 	mapSkin->_positions = MarchingSquares::GenerateMesh(*map);
-	mapSkin->_color = glm::vec4(0.5, 1, 0, 1);
+	mapSkin->_color = glm::vec4(1, 1, 1, 1);
 
 	for (GLuint i = 0; i < mapSkin->_positions.size(); i++)
 	{
@@ -149,15 +149,24 @@ int main()
 
 	// setup background
 	GLubyte backgroundImage[128][128][4];
+	glm::vec4 color1 = { 0, 249, 255, 255 };
+	glm::vec4 color2 = { 99, 0, 255, 255 };
+	glm::vec4 color3 = { 0, 0, 0, 255 };
 	for (int x = 0; x < 128; x++) {
 		for (int y = 0; y < 128; y++) {
-			int val = 255 - (map->value(x, y) * 20);
-			val = std::min(val, 255);
-			val = std::max(val, 0);
-			backgroundImage[y][x][0] = val / 4;
-			backgroundImage[y][x][1] = val / 2;
-			backgroundImage[y][x][2] = val;
-			backgroundImage[y][x][3] = val;
+			glm::lowp_u8vec4 col;
+			float val = std::max(0.0f, map->value(x, y));
+			if (val < 6) {
+				col = glm::mix(color1, color2, (val) / 6.0f);
+			} else if (val < 12) {
+				col = glm::mix(color2, color3, (val - 6.0f) / 6.0f);
+			} else {
+				col = color3;
+			}
+			backgroundImage[y][x][0] = col.x / 2.0f;
+			backgroundImage[y][x][1] = col.y / 2.0f;
+			backgroundImage[y][x][2] = col.z / 2.0f;
+			backgroundImage[y][x][3] = col.w / 2.0f;
 		}
 	}
 	Renderable* backgroundSkin = new Renderable();
