518f929e907342d6dc4cf7f3f9c4276a231e60f7
Matt Goerwell <Superninja246@hotmail.com>
2018-10-16 17:20:35 -0700

    Basic sound functionality

--- OpenGLTest/Sound.h
+++ OpenGLTest/Sound.h
@@ -6,7 +6,22 @@
 
 class Sound {
 public:
+    //methods
 	Sound();
-private:
+    ~Sound();
+    void SwitchTrack();
+    void PlayAudio();
+    void PauseAudio();
 
+    //variables
+    ALCdevice* device;
+    ALCcontext* context;
+    unsigned int bgmBuffer;
+    unsigned int bgmSource;
+
+private:
+    char * ReadWavFile(const char* fn, int& chan, int& samplerate, int& bps, int& size);
+    unsigned int determineFormat(int channels, int bps);
+    int convertToInt(char* buffer, int len);
+    bool isBigEndian();
 };
