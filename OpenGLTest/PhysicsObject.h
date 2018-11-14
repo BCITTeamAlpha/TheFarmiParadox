4692db833ef44d7742c09deac6025ee764cbec6c
limbermuffin <limbermuffin@gmail.com>
2018-11-13 21:28:14 -0800

    player movement with destructible terrain

--- OpenGLTest/PhysicsObject.h
+++ OpenGLTest/PhysicsObject.h
@@ -1,10 +1,15 @@
 #pragma once
 #include "GameObject.h"
+#include "Map.h"
 
 class PhysicsObject : public GameObject {
-public:
+	public:
 	PhysicsObject();
 	float mass;
+	float radius;
 	glm::vec2 velocity;
-private:
+
+	bool colliding_with_object(PhysicsObject &other, glm::vec2 &collision_normal);
+	bool colliding_with_map(Map & map, glm::vec2 acc, glm::vec2 &collision_normal);
+	private:
 };
