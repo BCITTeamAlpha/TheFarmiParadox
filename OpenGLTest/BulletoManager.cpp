8b51ff0f9e02c4bec256d2b25a67f49e2decdc04
Daniel Tian <daniel_tian2002@hotmail.com>
2018-12-06 16:34:00 -0800

    added some updates to bullet and character hit

--- OpenGLTest/BulletoManager.cpp
+++ OpenGLTest/BulletoManager.cpp
@@ -9,27 +9,31 @@ BulletoManager::BulletoManager(std::vector<Character*> playerList, std::vector<B
 
 void BulletoManager::UpdateBullet() {
 
-
+	
 	for each (auto &bullet in bulletListPtr) { //update bullet position
-		bullet->set_position(glm::vec2(bullet->get_position().x + tempSpeed, bullet->get_position().y)); tempSpeed += 0.00001f;
+		//bullet->set_position(glm::vec2(bullet->get_position().x + tempSpeed, bullet->get_position().y)); tempSpeed += 0.0001f;
+		//bullet->velocity = glm::vec2(20.0f, 0.0f);
 	}
 	
 
-	for (int i = 0; i < bulletListPtr.size(); i++) {	//for each bullet
+	for (int i = 0; i < bulletListPtr.size(); i++) {	//For every bullet in the game, check if they have hit any of the players 
 
-		Bulleto *bullet = bulletListPtr[i]; //get the bullet object from pointer
+		Bulleto *bullet = bulletListPtr[i]; 
 
-		for (int j = 0; j < playerListPtr.size();j++) { //look at every player and see if the bullet comes close
+		for (int j = 0; j < playerListPtr.size();j++) { 
 
 			Character *character = playerListPtr[j];
 
 			if (bullet->colliding_with_player(character->get_position())) {
 				character->health -= this->bulletDamage;
 				printf("A player got hit! health remaining: %d\n", character->health);
+				if (character->health < 0) {
+					character->renderable = NULL;
+					playerListPtr.erase(playerListPtr.begin() + j);
+				}
 				//std::cout << bullet->renderable.use_count() << "\n";
 				bullet->renderable = NULL; //delete bullet's renderable share pointer
 				bulletListPtr.erase(bulletListPtr.begin() + i);
-				tempSpeed = 0.0005f;
 				break;
 			}
 
@@ -46,6 +50,8 @@ void BulletoManager::SpawnBulleto(int x, int y) {
 	bullet->mass = 15;
 	bullet->radius = 2.0f;
 	bullet->set_position(glm::vec2(10, 10)); 	//bullet->set_position(physics->genSpawnPos(bullet->radius));
+
+	bullet->velocity = glm::vec2(40.0f, 10.0f);
 
 	Renderable *pSkin = new Renderable();
 	pSkin->z = 0;
